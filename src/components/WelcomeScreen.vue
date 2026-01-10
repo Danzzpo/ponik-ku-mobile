a8f6e1abceecc934245b68bda06df662155df723
Danzzpo <ramdanefendi095@gmail.com>
2026-01-10 14:31:46 +0700

    update

--- src/components/WelcomeScreen.vue
+++ src/components/WelcomeScreen.vue
@@ -7,7 +7,7 @@
       class="absolute left-1/2 top-[20%] -translate-x-1/2 w-[140px] h-auto object-contain drop-shadow-lg" 
     />
 
-    <div class="absolute left-0 bottom-0 w-full h-[400px] bg-[#17941F] rounded-tl-[60px] rounded-tr-none shadow-[0_-10px_30px_rgba(0,0,0,0.15)] dark:shadow-none">
+    <div class="absolute left-0 bottom-0 w-full h-[400px] bg-[#17941F] rounded-tl-[60px] rounded-tr-none shadow-[0_-10px_30px_rgba(0,0,0,0.15)] dark:shadow-none transition-colors duration-300">
       
       <h1 class="absolute left-[30px] top-[50px] font-bold text-[32px] leading-[1.2] text-white">
         Selamat Datang
@@ -18,19 +18,25 @@
       </p>
 
       <div class="absolute top-[180px] w-full px-[30px] flex justify-between gap-4">
+        
         <button 
           @click="$emit('change-screen', 'login')" 
-          class="flex-1 h-[50px] bg-gray-800 border border-white/30 rounded-[20px] flex items-center justify-center hover:bg-[#0f6315] transition active:scale-95 shadow-lg text-white"
+          class="flex-1 h-[50px] rounded-[20px] flex items-center justify-center shadow-lg transition active:scale-95 
+                 bg-white text-[#17941F] hover:bg-gray-100 
+                 dark:bg-gray-800 dark:text-green-400 dark:hover:bg-gray-700 dark:border dark:border-white/10"
         >
           <span class="font-bold text-[16px]">Masuk</span>
         </button>
 
         <button 
           @click="$emit('change-screen', 'register')" 
-          class="flex-1 h-[50px] bg-gray-800 border border-white/30 rounded-[20px] flex items-center justify-center hover:bg-[#0f6315] transition active:scale-95 shadow-lg text-white"
+          class="flex-1 h-[50px] rounded-[20px] flex items-center justify-center shadow-lg transition active:scale-95 
+                 bg-white text-[#17941F] hover:bg-gray-100 
+                 dark:bg-gray-800 dark:text-green-400 dark:hover:bg-gray-700 dark:border dark:border-white/10"
         >
           <span class="font-bold text-[16px]">Daftar</span>
         </button>
+
       </div>
 
       <div class="absolute bottom-[40px] left-0 w-full px-[30px] text-center">
